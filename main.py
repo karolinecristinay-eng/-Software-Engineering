67f89a265c888c2d728c82d17b7021848b084429
karolinecristinay-eng <karolinecristina.y@gmail.com>
2026-02-13 22:58:13 -0300

    Implementando função de criar tarefa

--- main.py
+++ main.py
@@ -7,4 +7,15 @@ class Tarefa:
         self.status = "Pendente"
 
     def __str__(self):
+
+        return f"[{self.id}] {self.titulo} - {self.responsavel} ({self.status})"
+class Tarefa:
+    def __init__(self, id, titulo, responsavel, prioridade):
+        self.id = id
+        self.titulo = titulo
+        self.responsavel = responsavel
+        self.prioridade = prioridade
+        self.status = "Pendente"
+
+    def __str__(self):
         return f"[{self.id}] {self.titulo} - {self.responsavel} ({self.status})"
