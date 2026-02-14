7b785d3db03ec60b6959b83399e695eea3ccb139
karolinecristinay-eng <karolinecristina.y@gmail.com>
2026-02-13 22:57:26 -0300

    setup inicial

---
+++ main.py
@@ -0,0 +1,10 @@
+class Tarefa:
+    def __init__(self, id, titulo, responsavel, prioridade):
+        self.id = id
+        self.titulo = titulo
+        self.responsavel = responsavel
+        self.prioridade = prioridade
+        self.status = "Pendente"
+
+    def __str__(self):
+        return f"[{self.id}] {self.titulo} - {self.responsavel} ({self.status})"
